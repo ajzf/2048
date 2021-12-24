8e5a2e6e5334c9d1ab526225d73a5c8393a99c2d
ajzf <96418509+ajzf@users.noreply.github.com>
2021-12-24 21:52:52 +0800

    Add files via upload

---
+++ 微信小程序/miniprogram/app.js
@@ -0,0 +1,37 @@
+//app.js
+App({
+  onLaunch: function () {
+    
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'testenvior-3g3nh8zkefd9791f',
+        traceUser: true,
+      })
+    }
+
+    this.globalData = {}
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
+  }
+})
